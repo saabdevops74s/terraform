acd0f65e0ac8ef666ec055236d59b30cd4dc003a
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-22 16:21:39 +0530

    Teraform Count

---
+++ count/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+    default = "ami-03265a0778a880afb" # this is the default value
+}
+
+variable "instance_names"{
+    type = list 
+    default = ["MongoDB", "Cart", "Catalogue", "User", "Redis", "MySQL", "RabbitMQ", "Shipping", "Payment", "Web"]
+}
+
+variable "zone_id" {
+    default = "Z08492172PZWNZ5Y0YV7Q"
+}
+
+variable "domain" {
+    default = "saabdevops.online"
+}
