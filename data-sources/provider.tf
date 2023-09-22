5fac76d49e95936cd2be2d77e8cda2cb8f14aac9
Shaik Ahammad <sk.ahammad25@gmail.com>
2023-09-22 16:24:34 +0530

    Teraform data-sources

---
+++ data-sources/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration 
+  #you can give access key and secret key here, but security problem
+  region = "ap-south-1"
+}
+
